e76de4e5acd4fdad1f6dc28766e760fce8b72f12
Muhammet Emin Akalan <05akalan57@gmail.com>
2023-10-26 15:45:11 +0300

    style: redesign user interface layout to align with the mockup design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,3 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx}'],
-  theme: {
-    extend: {
-      gridTemplateColumns: {
-        'main-grid': '1fr 3fr'
-      }
-    }
-  },
-  plugins: [require('daisyui')]
+  content: ['./src/**/*.{js,jsx}']
 }
