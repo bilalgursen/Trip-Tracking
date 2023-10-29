6c1d41927aabdb8245d53d55e8d4c36419735df5
bilalgursen <bilalgursen777@gmail.com>
2023-10-29 21:20:33 +0300

    Localstroge added

--- src/components/ContextProvider.js
+++ src/components/ContextProvider.js
@@ -1,26 +1,49 @@
 'use client'
 
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react';
 
-export const Context = createContext()
+export const Context = createContext();
 
 export const ContextProvider = ({ children }) => {
+  // Başlangıçta verileri boş olarak başlatın.
   const [data, setData] = useState({
     activeTravel: { city: 'Istanbul', day: 6 },
     travels: [{ city: 'Amasya', day: 4 }]
-  })
+  });
+
+  // LocalStorage'dan verileri çeken bir işlev oluşturun
+  const getDataFromLocalStorage = () => {
+    const storedData = localStorage.getItem('dataset');
+    if (storedData) {
+      return JSON.parse(storedData);
+    }
+    return data; // LocalStorage'da veri yoksa, mevcut veriyi kullan
+  };
+
+  // Sayfa yüklendiğinde LocalStorage'dan verileri çekin
+  useEffect(() => {
+    const storedData = getDataFromLocalStorage();
+    setData(storedData);
+  }, []);
+
+  // Verileri güncellediğinizde LocalStorage'a kaydedin
+  useEffect(() => {
+    localStorage.setItem('dataset', JSON.stringify(data));
+  }, [data]);
 
   const addData = travel => {
-    setData({ ...data, travels: [...data.travels, travel] })
-  }
+    const updatedData = { ...data, travels: [...data.travels, travel] };
+    setData(updatedData);
+  };
 
   const updateData = travel => {
-    setData({ activeTravel: travel, travels: [...data.travels] })
-  }
+    const updatedData = { activeTravel: travel, travels: [...data.travels] };
+    setData(updatedData);
+  };
 
   return (
     <Context.Provider value={{ data, addData, updateData }}>
       {children}
     </Context.Provider>
-  )
-}
+  );
+};
