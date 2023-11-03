62c72373c04a1c8117b9579b1fa55502cd7c481a
bilalgursen <bilalgursen777@gmail.com>
2023-11-03 19:01:31 +0300

    Added day + and - buttons

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -12,7 +12,7 @@ export default function Timeline() {
 
   return data.travels.length > 0 ? (
     <Swiper
-      className="cursor-grab select-none"
+      className="cursor-grab select-none !overflow-visible"
       spaceBetween={64}
       slidesPerView={2}
       breakpoints={{
@@ -25,7 +25,16 @@ export default function Timeline() {
             className="relative flex h-14 items-center justify-center rounded-lg border border-[#bfbfbf] bg-white font-semibold capitalize"
             onMouseOver={() => updateData(travel)}
           >
-            {travel.city} {travel.day} Gün
+            {travel.city}{' '}
+            <span className="absolute -left-4 -top-4  cursor-pointer rounded-lg border border-[#bfbfbf] bg-white p-1 px-3 text-xs">
+              +
+            </span>
+            <span className="pill absolute -top-4 rounded-lg border border-[#bfbfbf] bg-white p-1 text-xs">
+              {travel.day} Gün
+            </span>
+            <span className="absolute -right-4 -top-4 cursor-pointer rounded-lg border border-[#bfbfbf] bg-white p-1 px-3 text-xs">
+              -
+            </span>{' '}
             {i !== data.travels.length - 1 && (
               <span className="absolute left-full animate-pulse">
                 <div className="mx-1.5 flex space-x-1 text-2xl text-white">
